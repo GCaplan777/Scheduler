d5da8702d5a596192ab2ea4ae9e545722642befa
GCaplan <gabriel.caplan@gmail.com>
2020-07-11 02:18:47 -0400

    updated form

--- Develop/script.js
+++ Develop/script.js
@@ -12,6 +12,8 @@
 
 // ROW = time, text, submit
 
+// LOCAL Storage
+
 // DOM ELEMENTS ==============================
 var timeEl = document.getElementById("currentDay");
 $(".lead");
@@ -19,38 +21,81 @@ $(".lead");
 timeEl.innerHTML = moment().format("MMMM Do YYYY, h:mm:ss a");
 
 // CREATING BOOTSTRAP RESPONSIVE ROWS AND COLUMNS
-for (let i = 0; i < 9; i++) {
-  var divRow = document.createElement("div");
+var divRow;
+var ColumnLeft;
+var ColumnMiddle;
+var ColumnRight;
+var time;
+var textArea;
+var saveB;
+
+for (let i = 9; i < 18; i++) {
+  divRow = document.createElement("div");
   divRow.setAttribute("class", "row");
   document.body.appendChild(divRow);
-  console.log("check divRow");
+  //   console.log("check divRow");
   // create 3 columns inside each row
 
-  var ColumnEl1 = document.createElement("div");
-  ColumnEl1.setAttribute("class", "col-md-2");
-  var time = document.createElement("time");
-  time.setAttribute("class", "hour");
-  ColumnEl1.appendChild(time);
-  divRow.appendChild(ColumnEl1);
+  ColumnLeft = document.createElement("div");
+  ColumnLeft.setAttribute("class", "col-md-2 hour");
+  time = document.createElement("time");
+  //   time.setAttribute("class", "hour");
+  ColumnLeft.innerHTML = timeConversion(i);
+  ColumnLeft.appendChild(time);
+  divRow.appendChild(ColumnLeft);
 
-  var ColumnEl2 = document.createElement("div");
-  ColumnEl2.setAttribute("class", "col-md-8");
-  var textArea = document.createElement("TEXTAREA");
-  ColumnEl2.appendChild(textArea);
-  divRow.appendChild(ColumnEl2);
+  ColumnMiddle = document.createElement("div");
+  ColumnMiddle.setAttribute("class", "col-md-8 decription");
+  ColumnMiddle.setAttribute("id", "cm" + i);
+  textArea = document.createElement("textarea");
+  textArea.setAttribute("value", i);
+  ColumnMiddle.appendChild(textArea);
 
-  var ColumnEl3 = document.createElement("div");
-  ColumnEl3.setAttribute("class", "col-md-2");
-  var saveB = document.createElement("BUTTON");
+  divRow.appendChild(ColumnMiddle);
+
+  ColumnRight = document.createElement("div");
+  ColumnRight.setAttribute("class", "col-md-2");
+  saveB = document.createElement("button");
   saveB.setAttribute("class", "saveBtn");
-  ColumnEl3.appendChild(saveB);
-  divRow.appendChild(ColumnEl3);
+  saveB.setAttribute("value", i);
+  ColumnRight.appendChild(saveB);
+  divRow.appendChild(ColumnRight);
+
+  storedData(i);
 }
 
 // FUNCTIONS=========================
 
+// converting AM to PM for Left Column
+function timeConversion(num) {
+  var amToPm;
+  if (num <= 11) {
+    amToPm = num + "am";
+  } else if (num === 12) {
+    amToPm = num + "pm";
+  } else {
+    amToPm = num - 12 + "pm";
+  }
+  return amToPm;
+}
+
 // local storage
+function storedData(data) {
+  var entry = localStorage.getItem(data);
+  if (entry !== null) {
+    $("#cm" + data).text(entry);
+  }
+}
 
 // USER input===================
+
+saveB.on("click", function () {
+  event.preventDefault();
+  var entryID = $(this).val();
+  var entryText = $(this).parent().sibling().children(".description").val();
+  localStorage.setItem(entryID, entryText);
+  console.log(entryID);
+  console.log(entryText);
+});
 
 // Display based on user input===========================
